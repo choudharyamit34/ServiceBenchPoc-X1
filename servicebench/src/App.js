c20800e9c2d52f864b2f3557c4bf658f53d2acf8
Amit kumar <choudharyamit34@gmail.com>
2019-06-11 07:02:48 -0400

    added code to fetch data from node json api

--- servicebench/src/App.js
+++ servicebench/src/App.js
@@ -1,22 +1,37 @@
 import React, { Component } from 'react';
-
-import './App.css';
-import Header, {  } from "./components/templates/Header";
-import Footer from './components/templates/Footer';
-import Body from './components/templates/Body';
-
-
+import Table from './components/table/Table';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-          <Header />
-          <Body />
-          <Footer />
-      </div>
-    );
-  }
+    state = {
+        data: []
+    };
+    
+    componentDidMount() {
+        const url = "http://localhost:3007/ServiceJobs";
+
+        fetch(url)
+            .then(result => result.json())
+            .then(result => {
+                this.setState({
+                    data: result
+                })
+            });
+    }
+
+    render() {
+        const { data } = this.state;
+
+        return (
+            <div className="container">
+                <h1>Welcome to Service Bench</h1>
+                <p>Below are the lastest Service Jobs</p>
+                <Table
+                    characterData={data}
+                />             
+                
+            </div>
+        );
+    }
 }
 
 export default App;
