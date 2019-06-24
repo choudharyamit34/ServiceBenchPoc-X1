62a78e913290c79cfe87185f248342106d84fea7
amit kumar <choudharyamit34@gmail.com>
2019-06-24 16:12:38 +0530

    Added Sj details on Sj details component

--- servicebench/src/components/serviceJob/serviceJobDetails/sjDetail.js
+++ servicebench/src/components/serviceJob/serviceJobDetails/sjDetail.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
+import ReactDataGrid from 'react-data-grid';
 
-const data = [];
+const data2 = {};
 class SjDetail extends Component {
     state = {
-        data: [],
+        data: {},
         serviceJobNumber: ''
     };
     componentDidMount() {
@@ -14,7 +15,7 @@ class SjDetail extends Component {
             .then(result => result.json())
             .then(result => {
                 this.setState({
-                    data: result
+                    data: { ...result }
 
                 })
             });
@@ -22,37 +23,66 @@ class SjDetail extends Component {
     }
 
     render() {
-        const { data } = this.state;
-        console.log("data avialble in render ");
-        console.log(data);
+        var { data } = this.state;
+        //    const {data2}=data[0];
+        console.log("data avialble in render ", data);
         console.log("service job number in render ");
-        console.log(this.state.data.serviceJobNumber);
+        if (data[0] != undefined) {
+            console.log(data[0].serviceJobNumber);
+            data = data[0];
+            console.log("data copied", data);
+        }
+
         return (
             <div>
-                <div><button class="btn btn-link" onClick={this.props.history.goBack}>Go Back</button></div>
+                <div><button className="btn btn-link" onClick={this.props.history.goBack}>Go Back</button></div>
                 <div className="container">
                     <table className='table table-bordered table-hover'>
                         <tbody>
                             <tr>
-                                <td>serviceJobNumber</td>
-                                <td>{this.state.data.serviceJobNumber}</td>
+                                <td>Service Job Number</td>
+                                <td>{data.serviceJobNumber}</td>
+                                <td>Service Job Type</td>
+                                <td>{data.serviceJobType}</td>
                             </tr>
 
                             <tr>
-                                <td>Description</td>
-                                <td>{this.state.description}</td>
-                            </tr>
-
+                                <td>Service Job Date</td>
+                                <td>{data.serviceJobDate}</td>
+                                <td>Scheduled ServiceJob Date</td>
+                                <td>{data.scheduledServiceJobDate}</td>
+                            </tr>                            
                             <tr>
-                                <td>Price ($)</td>
-                                <td>${parseFloat(this.state.price).toFixed(2)}</td>
+                                <td>First Name</td>
+                                <td>{data.customerFirstName}</td>
+                                <td>Last name</td>
+                                <td>{data.customerLastName}</td>
                             </tr>
-
                             <tr>
-                                <td>Category</td>
-                                <td>{this.state.category_name}</td>
+                                <td>Mobile Phone</td>
+                                <td>{data.mobilePhone}</td>
+                                <td>Home Phone</td>
+                                <td>{data.homePhone}</td>
                             </tr>
-
+                            <tr>
+                                <td>City</td>
+                                <td>{data.city}</td>
+                                <td>State</td>
+                                <td>{data.state}</td>
+                            </tr>
+                            <tr>
+                                <td>Postal Code</td>
+                                <td>{data.postalCode}</td>
+                                <td>Addrerss Line 1 </td>
+                                <td>{data.addLine1}</td>
+                            </tr>
+                            <tr>
+                                <td>Address Line 2</td>
+                                <td>{data.addLine2}</td>
+                                <td>Addrerss Line 3 </td>
+                                <td>{data.addLine3}</td>
+                            </tr>
+                          
                         </tbody>
                     </table>
                 </div>
