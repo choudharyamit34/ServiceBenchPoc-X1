171c68c1aee13fd9178d95c5ad0eea25c5046da4
Piyush <choudharyamit34@gmail.com>
2019-07-02 16:59:34 +0530

    Added styling on header.

--- servicebench/src/components/templates/Footer.jsx
+++ servicebench/src/components/templates/Footer.jsx
@@ -1,41 +1,28 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 var style = {
-  backgroundColor: "gray",
+  backgroundColor: "white",
   borderTop: "1px solid #E7E7E7",
-  textAlign: "center",
+  textAlign: "right",
   padding: "20px",
   position: "fixed",
   left: "0",
   bottom: "0",
-  height: "60px",
+  height: "4%",
   width: "100%",
 }
 
-var phantom = {
-display: 'block',
-padding: '20px',
-height: '60px',
-width: '100%',
 
-}
+const Footer = () => (
 
-const Footer = () =>(
-//   <footer class="page-footer font-small blue pt-4">
-//   <div class="container-fluid text-center text-md-left">
-//       <div class="row">
-//          <div class="col-md-6 mt-md-0 mt-3">            
-//             <p>© ASURION 1994 - 2019 . ALL RIGHTS RESERVED | TERMS OF USE| PRIVACY POLICY</p>
-//          </div>
-//       </div>
-//    </div>
-// </footer>
-<div>
-            <div style={phantom} />
-            <div style={style}>
-                         <p>© ASURION 1994 - 2019 . ALL RIGHTS RESERVED | TERMS OF USE| PRIVACY POLICY</p>
-            </div>
-        </div>
+  <div >
+    <div style={style}>
+      <span>© Asurion 1994-2019. All Rights Reserved | </span>
+      <a href="https://www.asurion.com/terms-conditions/" target="_blank">Terms of Use</a>
+      <span>|</span>
+      <a href="https://d3pubnymrxzo91.cloudfront.net/privacy-policy/GDPR-ServiceBench-Privacy-Policy.html" target="_blank">Privacy Policy</a>
+    </div>
+  </div>
 
 )
 
--- servicebench/src/components/templates/Header.jsx
+++ servicebench/src/components/templates/Header.jsx
@@ -1,47 +1,55 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
-import  { Icon } from 'semantic-ui-react';
+import { Link } from 'react-router-dom';
+import { Icon } from 'semantic-ui-react';
 import LogoImage from '../../resources/imgs/SB_logo.png';
+import '../../Stylesheets/home.css';
 
 
 var logoStyle = {
-   background: `url(${LogoImage})`,
-   width : `22%`,
-   height:80,
-   marginTop :`-22px`,
-   cursor:'pointer'
+    background: `url(${LogoImage})`,
+    width: `22%`,
+    height: 80,
+    marginTop: `-22px`,
+    cursor: 'pointer'
 
 }
+var headerMenuColor = {
+    color: '#ecb215'
+}
 
-const Header = () =>(
+
+const Header = () => (
     <header>
-        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-                    <button type="button" data-target="#navbarCollapse"
-                        data-toggle="collapse" className="navbar-toggler">
-                        <span className="navbar-toggler-icon"></span>
-                    </button>
-                    <div style={logoStyle} />
-                <div id="navbarCollapse" className="collapse navbar-collapse">
-                    <ul className="navbar-nav">
-                        <li className="nav-item"><Link className='nav-link' to='/home'>
-                        <Icon name='home' size='big' /><br />Home</Link></li>
-                        <li className="nav-item"><Link className='nav-link' to='/serviceJobs'>
-                        <Icon name='tasks' size='big'/><br />Jobs</Link></li>
-                        <li className="nav-item"><Link className='nav-link' to='/parts'>
-                        <Icon name='cogs' size='big'/><br />Parts</Link></li>
-                        <li className="nav-item"><Link className='nav-link' to='/claims'>
-                        <Icon name='file alternate' size='big'/><br />Claims</Link></li>
-                        <li className="nav-item"><Link className='nav-link' to='/home'>
-                        <Icon name='copy' size='big'/><br />Documents</Link></li>
-                        <li className="nav-item"><Link className='nav-link' to='/home'>
-                        <Icon name='handshake' size='big'/><br />Contracts</Link></li>
-                        <li className="nav-item"><Link className='nav-link' to='/home'>
-                        <Icon name='edit' size='big'/><br />Registrations</Link></li>
-                        <li className="nav-item"><Link className='nav-link' to='/home'>
-                        <Icon name='book' size='big'/><br />Reports</Link></li>
-                    </ul>
-                </div>
-            </nav>
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark height_75_px">
+            <button type="button" data-target="#navbarCollapse"
+                data-toggle="collapse" className="navbar-toggler">
+                <span className="navbar-toggler-icon"></span>
+            </button>
+            <div style={logoStyle} />
+            <div id="navbarCollapse" className="collapse navbar-collapse">
+                <ul className="navbar-nav">
+                    <li className="nav-item " className="headerIcon" style={{ textAlign: "center" }}>
+                        <Link className='nav-link' to='/home'>
+                            <Icon name='home' size='big' className="colorLogo"/>
+                            <br /><span>Home</span></Link>
+                    </li>
+                    <li className="nav-item" className="headerIcon" style={{ textAlign: "center" }} ><Link className='nav-link' to='/serviceJobs'>
+                        <Icon name='tasks' size='big' className="colorLogo" /><br />Jobs</Link></li>
+                    <li className="nav-item" className="headerIcon"  style={{ textAlign: "center" }}><Link className='nav-link' to='/parts'>
+                        <Icon name='cogs' size='big' className="colorLogo" /><br />Parts</Link></li>
+                    <li className="nav-item" className="headerIcon"  style={{ textAlign: "center" }}><Link className='nav-link' to='/claims'>
+                        <Icon name='file alternate' size='big' className="colorLogo" /><br />Claims</Link></li>
+                    <li className="nav-item" className="headerIcon"  style={{ textAlign: "center" }}><Link className='nav-link' to='/home'>
+                        <Icon name='copy' size='big' className="colorLogo" /><br />Documents</Link></li>
+                    <li className="nav-item" className="headerIcon"  style={{ textAlign: "center" }}><Link className='nav-link' to='/home'>
+                        <Icon name='handshake' size='big' className="colorLogo" /><br />Contracts</Link></li>
+                    <li className="nav-item" className="headerIcon"  style={{ textAlign: "center" }}><Link className='nav-link' to='/home'>
+                        <Icon name='edit' size='big' className="colorLogo" /><br />Registrations</Link></li>
+                    <li className="nav-item" className="headerIcon"  style={{ textAlign: "center" }}><Link className='nav-link' to='/home'>
+                        <Icon name='book' size='big' className="colorLogo" /><br />Reports</Link></li>
+                </ul>
+            </div>
+        </nav>
     </header>
 )
 
