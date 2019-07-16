56697adf21dfe32da6665e1eb7be74eaa1136003
Piyush <choudharyamit34@gmail.com>
2019-07-16 15:39:06 +0530

    corrected footer links for social icon.

--- servicebench/src/components/templates/Footer.jsx
+++ servicebench/src/components/templates/Footer.jsx
@@ -19,11 +19,11 @@ const Footer = () => (
         <div className="float_left">
           <span>© Asurion 1994-2019. All Rights Reserved |</span> <a href="">Contact Us</a>
         </div>
-        <div className="marginTop_minus_10  float_right" >
-          <a href="https://www.facebook.com/bootsnipp"><i id="social-fb" className="fa fa-facebook-square fa-3x social"></i></a>
-          <a href="https://twitter.com/bootsnipp"><i id="social-tw" className="fa fa-twitter-square fa-3x social"></i></a>
-          <a href="https://plus.google.com/+Bootsnipp-page"><i id="social-gp" className="fa fa-google-plus-square fa-3x social"></i></a>
-          <a href="mailto:#"><i id="social-em" className="fa fa-envelope-square fa-3x social"></i></a>
+        <div className="marginTop_minus_10  float_right" >          
+          <a href="https://www.facebook.com/ServiceBench1"><i id="social-fb" className="fa fa-facebook-square fa-3x social"></i></a>
+          <a href="https://twitter.com/servicebench1"><i id="social-tw" className="fa fa-twitter-square fa-3x social"></i></a>
+          <a href="https://www.youtube.com/user/ServiceBench"> <i id="social-gp" className="fa fa-youtube-square fa-3x social"></i></a>
+          <a href="https://www.linkedin.com/company/servicebench"><i id="social-tw" className="fa fa-linkedin-square fa-3x social"></i></a>          
         </div>
       </div>
 
